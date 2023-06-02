07fee6ada34873e8899d3953b60dcc88754b3a8d
Shreedhar <shreedhar@Shreedhars-Laptop>
2023-06-02 15:49:14 +0545

    managed permissions

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:music_player/core/router/app_router.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 @RoutePage()
 class SplashScreen extends StatefulWidget {
@@ -14,44 +15,93 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   bool? hasPermission;
   checkForPermission() async{
-    hasPermission = await OnAudioQuery().checkAndRequest(
+    return await OnAudioQuery().checkAndRequest(
       retryRequest: false
     );
-    hasPermission ?? false ? setState((){}) : null;
   }
   @override
   void initState() {
-    checkForPermission();
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback( (timeStamp) {
-      Future.delayed(
-        const Duration(milliseconds: 100),
-        (){
-          if(hasPermission ?? false){
-            context.router.replace(
-              const BottombarRoute()
-            );
-          }
+  }
+
+  routeToHome(){
+    Future.delayed(
+      const Duration(milliseconds: 100),
+      (){
+        if(hasPermission ?? false){
+          context.router.replace(
+            const BottombarRoute()
+          );
         }
+      }
+    );
+  }
+
+  checkForExternalPermission() async{
+    PermissionStatus status = await Permission.manageExternalStorage.request();
+    if( status.isGranted){
+      hasPermission = true;
+      routeToHome();
+    }else if(status.isPermanentlyDenied){
+      openAppSettings().then((value) 
+      => setState((){})
       );
-    });
+    }
+    
+    return status;
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF0A091E),
-      body: hasPermission ?? false ?
-      const Center(
-        child: Text(' Splash Screen',style: TextStyle(color: Colors.white),),
-      )
-      : Center(
-        child: InkWell(
-          onTap: ()async{
-            checkForPermission();
-          },
-          child: const Text("Request permission",style: TextStyle(color: Colors.white),),
-        ),
+      body: FutureBuilder(
+        future: checkForPermission(),
+        builder: (ctx,snapshot){
+          if(snapshot.hasData){
+            var permissionStatus = snapshot.data;
+            if(permissionStatus == true){
+              hasPermission = true;
+              routeToHome();
+              return const Center(
+                child:  Text(
+                  'Splash Screen', style: TextStyle(color: Colors.white),
+                ),
+              );
+            }else{
+              return Center(
+                child: InkWell(
+                  onTap: ()async{
+                    print("Check for Permission");
+                    checkForExternalPermission();
+                  },
+                  child: const Text("Request permission",style: TextStyle(color: Colors.white),),
+                ),
+              );
+            }
+          }else{
+            return Center(
+              child: InkWell(
+                onTap: ()async{
+                  checkForExternalPermission();
+                },
+                child: const Text("Request permission",style: TextStyle(color: Colors.white),),
+              ),
+            );
+          }
+        },
       ),
+      // body: hasPermission ?? false ?
+      // const Center(
+      //   child: Text(' Splash Screen',style: TextStyle(color: Colors.white),),
+      // )
+      // : Center(
+      //   child: InkWell(
+      //     onTap: ()async{
+      //       checkForPermission();
+      //     },
+      //     child: const Text("Request permission",style: TextStyle(color: Colors.white),),
+      //   ),
+      // ),
     );
   }
 }
